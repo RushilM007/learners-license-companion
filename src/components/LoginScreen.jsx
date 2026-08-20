9ba28f4465fe84b63f68fdba2dc168b75c90669e
Rushil Motamarri <mrushil007@gmail.com>
2026-08-20 18:57:47 +0530

    reversed some changes

--- src/components/LoginScreen.jsx
+++ src/components/LoginScreen.jsx
@@ -31,7 +31,7 @@ export default function LoginScreen(){
         <main>
             <div id = "AuthCard">
                 <h3 id = "AuthHeader">Sign in to Learner's License Companion</h3>
-                <form id = "AuthForm" onClick = {onSubmit}>
+                <form id = "AuthForm">
                     <label className = "AuthLabel" htmlFor="email"> Email Address </label>
                     
                     <input 
@@ -54,7 +54,7 @@ export default function LoginScreen(){
 
                     <Link to="/ForgotPassword" className = "AuthHyperLink">Forgot Password?</Link>
 
-                    <button className = "AuthButton" type = "submit" >Sign In</button>
+                    <button className = "AuthButton" onClick = {onSubmit}>Sign In</button>
 
                     <p className = "AuthAlternateOptionLiner">Don't have an account? <Link to="/SignUp" className = "AuthHyperLink">Sign Up</Link></p>
 
